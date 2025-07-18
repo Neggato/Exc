73589b7c3f5d92f837e05de1e1ed54bb09770b7d
Usuário <Usurio@DESKTOP-AR0HMME>
2025-07-18 17:54:32 -0300

    Adicionar arquivos de projeto.

---
+++ Exc/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Example
+{
+    public static void Main(string[] args)
+    {
+        Random random = new Random();
+
+        //gerar um numero aleatório entre 1 e 10
+        int numero2 = random.Next(5, 16);
+        Console.WriteLine($"Número aleatório entre 5 e 15: {numero2}");
+
+    
+    }
+}
